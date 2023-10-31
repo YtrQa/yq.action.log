fc9b2426647c9ad3795df22b2ba61d1f3d40c830
Secrieru Alexander <alexander.secrieru@katoennatie.com>
2023-10-31 11:47:28 +0100

    added comments

--- src/ActivityEventLogSystem/Interfaces/IActivityLog.cs
+++ src/ActivityEventLogSystem/Interfaces/IActivityLog.cs
@@ -9,11 +9,17 @@ namespace ActivityEventLogSystem.Interfaces
 	public interface IActivityLog
 	{
 		Guid Id { get; set; }
+		//Activity type is the name of the activity
 		string ActivityType { get; set; }
+		//Actor type is the name of the actor
 		string ActorType { get; set; }
-		string ActorId { get; set; }
+		//Actor is the id of the actor
+		string Actor { get; set; }
+		//Target is the id of the target
 		string Target { get; set; }
+		//Payload is the json of the changes
 		string Payload { get; set; }
+		//Timestamp is the time of the activity
 		DateTime Timestamp { get; set; }
 	}
 }
