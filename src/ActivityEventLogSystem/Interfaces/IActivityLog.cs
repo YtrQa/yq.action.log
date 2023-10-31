96f3c50ad95e97bd4a9ab2dc6e0ee73d34bcb877
Secrieru Alexander <alexander.secrieru@katoennatie.com>
2023-10-31 11:30:43 +0100

    compare objects

---
+++ src/ActivityEventLogSystem/Interfaces/IActivityLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityEventLogSystem.Interfaces
+{
+	public interface IActivityLog
+	{
+		Guid Id { get; set; }
+		string ActivityType { get; set; }
+		string ActorType { get; set; }
+		string ActorId { get; set; }
+		string Target { get; set; }
+		string Payload { get; set; }
+		DateTime Timestamp { get; set; }
+	}
+}
